54fba5ef5234c6fa8cf9bd0e3621972723e9127a
Mikhail Filchushkin <webestet@gmail.com>
2023-07-29 14:53:49 +0300

    DSN generation imroved

--- page-scraper/src/services/mongoose-config.service.ts
+++ page-scraper/src/services/mongoose-config.service.ts
@@ -5,8 +5,6 @@ import {
   MongooseOptionsFactory,
 } from '@nestjs/mongoose';
 
-import { Mode } from '../constants';
-
 
 @Injectable()
 export class MongoConfigService implements MongooseOptionsFactory {
@@ -15,11 +13,33 @@ export class MongoConfigService implements MongooseOptionsFactory {
   ) {
   }
 
+  private username = this.configService.get('MONGO_INITDB_ROOT_USERNAME');
+  private password = this.configService.get('MONGO_INITDB_ROOT_PASSWORD');
+  private database = this.configService.get('MONGO_INITDB_DATABASE');
+  private protocol = this.configService.get('MONGO_PROTOCOL')
+    ? this.configService.get('MONGO_PROTOCOL')
+    : 'mongodb';
+  private host = this.configService.get('MONGO_HOST');
+  private port = this.configService.get('MONGO_PORT')
+    ? ':' + this.configService.get('MONGO_PORT')
+    : '';
+  private queryParamReplicaSet = this.configService.get('MONGO_RS')
+    ? '&replicaSet=' + this.configService.get('MONGO_RS')
+    : '';
+  private queryParamRetryWrites = this.configService.get('MONGO_RETRY_WRITES') === 'true'
+    ? '&retryWrites=' + this.configService.get('MONGO_RETRY_WRITES')
+    : '';
+  private queryParamW = this.configService.get('MONGO_W')
+    ? '&w=' + this.configService.get('MONGO_W')
+    : '';
+  private queryParamStringExists = Boolean(this.queryParamReplicaSet) || Boolean(this.queryParamRetryWrites) || Boolean(this.queryParamW);
+  private queryString = this.queryParamStringExists
+    ? `${this.queryParamReplicaSet}${this.queryParamRetryWrites}${this.queryParamW}`.replace(/^&/, '?')
+    : '';
+
   public createMongooseOptions(): MongooseModuleOptions {
     return {
-      uri: this.configService.get('MODE') === Mode.Dev
-        ? `mongodb://${this.configService.get('MONGO_INITDB_ROOT_USERNAME')}:${this.configService.get('MONGO_INITDB_ROOT_PASSWORD')}@localhost:${this.configService.get('MONGO_PORT')}/${this.configService.get('MONGO_INITDB_DATABASE')}?authSource=admin${this.configService.get('MONGO_RS') ? '&replicaSet=' + this.configService.get('MONGO_RS') : ''}&ssl=false`
-        : `mongodb://${this.configService.get('MONGO_INITDB_ROOT_USERNAME')}:${this.configService.get('MONGO_INITDB_ROOT_PASSWORD')}@localhost:${this.configService.get('MONGO_PORT')}/${this.configService.get('MONGO_INITDB_DATABASE')}`,
+      uri: `${this.protocol}://${this.username}:${this.password}@${this.host}${this.port}${this.queryString}`,
     };
   }
 }
