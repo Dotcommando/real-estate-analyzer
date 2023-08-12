38225d4a88324cc7a94f927f8f8d3b7b96816ebf
Mikhail Filchushkin <webestet@gmail.com>
2023-08-12 15:28:35 +0300

    Dataframe prepared for analysis

--- analysis/classes/data_analyser.py
+++ analysis/classes/data_analyser.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 import pandas as pd
 from datetime import datetime
 from bson.objectid import ObjectId
@@ -42,19 +43,136 @@ class DataAnalyser:
 
         return df
 
-    def convert_includes(self, df):
-        # Проверяем, есть ли столбец 'includes' в датафрейме
-        if 'includes' not in df.columns:
+    def convert_included(self, df):
+        # Проверяем, есть ли столбец 'included' в датафрейме
+        if 'included' not in df.columns:
             return df
 
-        # Преобразование списков в includes в серии и затем преобразование их в dummy-переменные
-        includes_dummies = df['includes'].apply(pd.Series).stack().str.get_dummies().sum(level=0)
+        # Фильтрация значений 'included', чтобы исключить строки из одного символа
+        df['included'] = df['included'].apply(lambda x: [item for item in x if len(item) > 1])
+
+        # Преобразование списков в included в серии и затем преобразование их в dummy-переменные
+        included_dummies = df['included'].apply(pd.Series).stack().str.get_dummies().sum(level=0)
 
         # Присоединение этих dummy-переменных к исходному датафрейму
-        df = pd.concat([df.drop('includes', axis=1), includes_dummies], axis=1)
+        df = df.drop('included', axis=1).join(included_dummies)
 
-        # Замена NaN на False
-        df.fillna(False, inplace=True)
+        # Замена NaN на False и преобразование 1 и 0 в True и False
+        for column in included_dummies.columns:
+            df[column] = df[column].fillna(0).astype(bool)
+
+        return df
+
+    def analyse_city_district(self, df):
+        # Группировка по городу и району и подсчёт количества уникальных сочетаний
+        combinations = df.groupby(['city', 'district']).size().reset_index(name='counts')
+
+        # Сортировка по количеству уникальных сочетаний в порядке убывания
+        combinations = combinations.sort_values('counts', ascending=False)
+
+        # Выводим на экран уникальные сочетания городов и районов и их количество
+        print(f'\nCollection {self.collection.name}')
+        for index, row in combinations.iterrows():
+            print(f'"{row["city"]} - {row["district"]}": {row["counts"]}')
+
+    def analyse_boolean_columns(self, df):
+        # Число строк в датафрейме
+        total_count = df.shape[0]
+
+        # Фильтруем только колонки с булевыми значениями
+        bool_df = df.select_dtypes(include=[bool])
+
+        # Получаем сумму значений в каждой колонке и преобразуем в словарь
+        column_counts = bool_df.sum().to_dict()
+
+        # Печатаем отношение количества True значений к общему количеству для каждой колонки
+        for column, count in column_counts.items():
+            print(f"{column}: {count}/{total_count}")
+
+        return df
+
+    def analyse_district_sqm_costs(self, df):
+        # Создаем новую колонку с ценой за квадратный метр
+        df['price_per_sqm'] = df['price'] / df['property-area']
+
+        # Группируем по городу и району и получаем среднее значение цены за квадратный метр
+        city_district_avg_price = df.groupby(['city', 'district'])['price_per_sqm'].mean()
+
+        # Подсчет числа объектов для каждой группы
+        city_district_counts = df.groupby(['city', 'district']).size()
+
+        # Сортируем по средней цене за квадратный метр в порядке убывания
+        city_district_avg_price = city_district_avg_price.sort_values(ascending=False)
+
+        # Печатаем ранжированный список городов и районов
+        print(f'\nCity-District Average Price Per Square Meter (in collection {self.collection.name}):')
+        for index, value in city_district_avg_price.items():
+            city, district = index
+            count = city_district_counts[city][district]
+
+            if count >= 4:
+                print(f'{city} - {district}: {round(value, 2)} (based on data on {count} objects)')
+
+        return df
+
+    def remove_fields(self, df, fields_to_exclude):
+        return df.drop(columns=[field for field in fields_to_exclude if field in df.columns], errors='ignore')
+
+    def drop_if_null(self, df, field_name, empty_value, valid_types):
+        """
+        Удаляет строки из DataFrame, если значение в указанной колонке соответствует одному из пустых значений
+        или не соответствует указанным типам.
+
+        Args:
+        - df (pd.DataFrame): DataFrame, из которого необходимо удалить строки.
+        - field_name (str): Имя колонки для проверки.
+        - empty_value (list): Список значений, которые считаются "пустыми".
+        - valid_types (list): Список типов, которые считаются допустимыми для этой колонки.
+
+        Returns:
+        - pd.DataFrame: Обновленный DataFrame без строк, соответствующих условиям.
+        """
+        # Условие для удаления строк, если поле отсутствует
+        condition_missing = df[field_name].isnull()
+
+        # Условие для удаления строк на основе "пустых" значений
+        condition_empty = df[field_name].isin(empty_value)
+
+        # Условие для удаления строк на основе типов
+        condition_type = df[field_name].apply(lambda x: not any(isinstance(x, eval(type_)) for type_ in valid_types))
+
+        # Объединение условий
+        combined_condition = condition_missing | condition_empty | condition_type
+
+        return df[~combined_condition]
+
+    def set_default_value(self, df, field_name, valid_values_or_type, default_value=None):
+        """
+        Устанавливает значение по умолчанию для указанной колонки, если текущее значение
+        в колонке не соответствует списку допустимых значений или указанному типу.
+
+        Args:
+        - df (pd.DataFrame): DataFrame, который нужно обновить.
+        - field_name (str): Имя колонки для проверки и обновления.
+        - valid_values_or_type (list or str): Список допустимых значений или строка с именем допустимого типа.
+        - default_value (optional): Значение по умолчанию, которое следует установить, если текущее значение недопустимо.
+
+        Returns:
+        - pd.DataFrame: Обновленный DataFrame с установленными значениями по умолчанию.
+        """
+
+        if isinstance(valid_values_or_type, list):
+            condition = ~df[field_name].isin(valid_values_or_type)
+
+            # Если значение по умолчанию не указано, выберите наиболее часто встречающееся значение из списка допустимых значений
+            if default_value is None:
+                default_value = df[field_name].value_counts().loc[lambda x: x.index.isin(valid_values_or_type)].idxmax()
+        else:
+            valid_type = eval(valid_values_or_type)
+            condition = ~df[field_name].apply(lambda x: isinstance(x, valid_type))
+
+        # Применить значение по умолчанию к строкам, которые удовлетворяют условию
+        df.loc[condition, field_name] = default_value
 
         return df
 
@@ -89,12 +207,13 @@ class DataAnalyser:
         # Создаем датафрейм из собранных данных
         df = pd.DataFrame(data)
 
-        columns_to_drop = ['reference-number', 'registration-number', 'registration-block', 'square-meter-price']
+        fields_to_exclude = [
+            'url', 'ad_id', 'title', 'description', 'currency',
+            'reference-number', 'registration-number', 'registration-block',
+            'property-area-unit', 'coords', 'mode', '__v',
+            'plot-area-unit', 'square-meter-price' ]
 
-        # Проверяем наличие колонки перед её удалением
-        for column in columns_to_drop:
-            if column in df.columns:
-                df = df.drop(columns=[column])
+        df = self.remove_fields(df, fields_to_exclude)
 
         # Создаем маску, где каждое значение True, если соответствующий элемент в 'district' является строкой, и False в противном случае
         mask = df['district'].apply(lambda x: isinstance(x, str))
@@ -110,51 +229,66 @@ class DataAnalyser:
         df = df[df['property-area'] != 0]
 
         df = self.clean_districts(df)
-        df = self.convert_includes(df)
+        df = self.convert_included(df)
+        df = self.remove_fields(df, [ 'included' ])
 
-        # # Группировка по городу и району и подсчёт количества уникальных сочетаний
-        # combinations = df.groupby(['city', 'district']).size().reset_index(name='counts')
-        #
-        # # Сортировка по количеству уникальных сочетаний в порядке убывания
-        # combinations = combinations.sort_values('counts', ascending=False)
-        #
-        # # Выводим на экран уникальные сочетания городов и районов и их количество
-        # print(f'\nCollection {self.collection.name}')
-        # for index, row in combinations.iterrows():
-        #     print(f'"{row["city"]} - {row["district"]}": {row["counts"]}')
-        #
-        # # Число строк в датафрейме
-        # total_count = df.shape[0]
-        #
-        # # Фильтруем только колонки с булевыми значениями
-        # bool_df = df.select_dtypes(include=[bool])
-        #
-        # # Получаем сумму значений в каждой колонке и преобразуем в словарь
-        # column_counts = bool_df.sum().to_dict()
-        #
-        # # Печатаем отношение количества True значений к общему количеству для каждой колонки
-        # for column, count in column_counts.items():
-        #     print(f"{column}: {count}/{total_count}")
+        ignored_fields = [
+            'url', 'ad_id', 'title', 'description', 'currency',
+            'reference-number', 'registration-number', 'registration-block',
+            'property-area-unit', 'coords', 'mode', '_id', '__v', 'active_dates',
+            'plot-area-unit', 'included', 'publish_date' ]
 
-        # Создаем новую колонку с ценой за квадратный метр
-        df['price_per_sqm'] = df['price'] / df['property-area']
+        energy_efficiency_values = [ 'A+++', 'A++', 'A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'E+', 'E', 'N/A', 'In Progress' ]
 
-        # Группируем по городу и району и получаем среднее значение цены за квадратный метр
-        city_district_avg_price = df.groupby(['city', 'district'])['price_per_sqm'].mean()
+        df = self.drop_if_null(df, 'price', [ 0 ], [ 'int', 'float' ])
+        df = self.drop_if_null(df, 'property-area', [ 0 ], [ 'int' ])
+        df = self.set_default_value(df, 'online-viewing', [ 'Yes', 'No' ], [ 'No' ])
+        df = self.set_default_value(df, 'condition', [ 'Brand new', 'Resale', 'Under construction' ], [ 'Resale' ])
+        df = self.set_default_value(df, 'energy-efficiency', energy_efficiency_values, [ 'In Progress' ])
+        df = self.set_default_value(df, 'type', 'str')
+        df = self.set_default_value(df, 'parking', [ 'No', 'Covered', 'Uncovered' ], 'No')
+        df = self.set_default_value(df, 'furnishing', [ 'Fully Furnished', 'Semi-Furnished', 'Unfurnished', 'Appliances οnly' ], 'Unfurnished')
+        df = self.set_default_value(df, 'air-conditioning', [ 'Full, all rooms', 'Partly', 'No' ], 'No')
+        df = self.set_default_value(df, 'bedrooms', 'int', 1)
+        df = self.set_default_value(df, 'bathrooms', 'int', 1)
 
-        # Подсчет числа объектов для каждой группы
-        city_district_counts = df.groupby(['city', 'district']).size()
+        df = self.field_analysis(df, ignored_fields)
 
-        # Сортируем по средней цене за квадратный метр в порядке убывания
-        city_district_avg_price = city_district_avg_price.sort_values(ascending=False)
+        # Выведем первые 5 строк датафрейма
+        print(df.head())
 
-        # Печатаем ранжированный список городов и районов
-        print(f'\nCity-District Average Price Per Square Meter (in collection {self.collection.name}):')
-        for index, value in city_district_avg_price.items():
-            city, district = index
-            count = city_district_counts[city][district]
+        # Список интересующих колонок
+        columns_of_interest = [ 'Alarm', 'Attic/Loft', 'Balcony', 'Elevator', 'Fireplace', 'Garden', 'Playroom', 'Pool', 'Storage room' ]
 
-            if count >= 4:
-                print(f'{city} - {district}: {round(value, 2)} (based on data on {count} objects)')
+        # Отфильтруйте строки, где хотя бы одна из указанных колонок имеет тип float
+        float_rows = df[df[columns_of_interest].applymap(lambda x: isinstance(x, float)).any(axis=1)].head()
+
+        print(float_rows)
+
+        return df
+
+    def field_analysis(self, df, ignored_fields):
+        print(f'\n')
+        print(f'\nAnalysis of collection {self.collection.name}:')
+        print(f'\nColumn name - type(s) - diff. values/empty values/common notes number')
+
+        for column in df.columns:
+            if column in ignored_fields:
+                continue
+
+            if df[column].apply(type).eq(list).any():
+                print(f"{column} contains lists, skipping unique value calculation")
+                continue
+
+            # Определение типа данных
+            data_types = set(df[column].apply(type).unique())
+
+            # Число разных значений
+            unique_values = len(df[column].unique())
+
+            # Число пустых значений
+            null_values = df[column].isnull().sum()
+
+            print(f"{column} - {', '.join([str(t.__name__) for t in data_types])} - {unique_values}/{null_values}/{len(df)}")
 
         return df
