67b22bcf223a8e080b394d997de1d238428840c4
Mikhail Filchushkin <webestet@gmail.com>
2023-08-19 14:13:38 +0300

    New statistics added

--- analysis/classes/median_price_calculator.py
+++ analysis/classes/median_price_calculator.py
@@ -9,28 +9,69 @@ class MedianPriceCalculator:
         self.mode = mode
 
     def analyse_city_district(self, df):
-        # Группировка по городу и району и подсчёт медианной и средней цены за квадратный метр
-        stats_df = df.groupby(['city', 'district'])['price_per_sqm'].agg(['median', 'mean', 'count']).reset_index()
+        # Группировка по городу и району и подсчёт различных статистик
+        stats_df = df.groupby(['city', 'district']).agg({
+            'price_per_sqm': [
+                ('median-sqm', 'median'),
+                ('mean-sqm', 'mean'),
+                ('count', 'count'),
+                ('percentile-25-sqm', lambda x: x.quantile(0.25)),
+                ('percentile-75-sqm', lambda x: x.quantile(0.75))
+            ],
+            'property-area': [('total-area', 'sum')],
+            'price': [
+                ('total-price', 'sum'),
+                ('median-price', 'median'),
+                ('mean-price', 'mean'),
+                ('percentile-25-price', lambda x: x.quantile(0.25)),
+                ('percentile-75-price', lambda x: x.quantile(0.75))
+            ]
+        }).reset_index()
+
+        # Поскольку мы использовали многомерные заголовки, нам нужно их "сгладить"
+        stats_df.columns = [col[1] if col[1] else col[0] for col in stats_df.columns.values]
 
         # Сортировка по городу и медианной цене за квадратный метр в порядке убывания
-        stats_df = stats_df.sort_values(['city', 'median'], ascending=[True, False])
+        stats_df = stats_df.sort_values(['city', 'median-sqm'], ascending=[True, False])
 
         return stats_df
 
-    def print_analysis_of_city_district(self, median_prices_df, min_objects_threshold = 0, mode: str = "prod"):
+    def print_analysis_of_city_district(self, stats_df, min_objects_threshold = 0, mode: str = "prod"):
         if mode == "prod":
             return
 
-        temp_df = median_prices_df.copy()
+        temp_df = stats_df.copy()
         temp_df['  City'] = temp_df['city']
         temp_df['  District'] = temp_df['district']
-        temp_df['  Median price/sqm'] = temp_df['median'].apply(lambda x: f"{x:.2f}")
-        temp_df['  Avg price/sqm'] = temp_df['mean'].apply(lambda x: f"{x:.2f}")
+        temp_df['  Med sqm'] = temp_df['median-sqm'].apply(lambda x: f"{x:.2f}")
+        temp_df['  Avg sqm'] = temp_df['mean-sqm'].apply(lambda x: f"{x:.2f}")
         temp_df['  Objects'] = temp_df['count']
+        temp_df['  sqm 25 Perc.'] = temp_df['percentile-25-sqm'].apply(lambda x: f"{x:.2f}")
+        temp_df['  sqm 75 Perc.'] = temp_df['percentile-75-sqm'].apply(lambda x: f"{x:.2f}")
+        temp_df['  Ttl Area'] = temp_df['total-area'].round().astype(int).astype(str)
+        temp_df['  Ttl Price'] = temp_df['total-price'].round().astype(int).astype(str)
+        temp_df['  Med Price'] = temp_df['median-price'].round().astype(int).astype(str)
+        temp_df['  Avg Price'] = temp_df['mean-price'].round().astype(int).astype(str)
+        temp_df['  25 Perc.'] = temp_df['percentile-25-price'].round().astype(int).astype(str)
+        temp_df['  75 Perc.'] = temp_df['percentile-75-price'].round().astype(int).astype(str)
 
         temp_df = temp_df[temp_df['  Objects'] >= min_objects_threshold]
 
-        columns_to_show = ['  City', '  District', '  Median price/sqm', '  Avg price/sqm', '  Objects']
+        columns_to_show = [
+            '  City',
+            '  District',
+            '  Med sqm',
+            '  Avg sqm',
+            '  sqm 25 Perc.',
+            '  sqm 75 Perc.',
+            '  Objects',
+            '  Ttl Area',
+            '  Ttl Price',
+            '  Med Price',
+            '  Avg Price',
+            '  25 Perc.',
+            '  75 Perc.'
+        ]
 
         print(f"\nDetailed prices for City-Districts in {self.collection.name}:\n")
         print(temp_df[columns_to_show].to_string(index=False))
