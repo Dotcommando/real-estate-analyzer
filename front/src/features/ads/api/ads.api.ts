78ea5ab16ab74c7563af999267e0a8093619b9cc
Mikhail Filchushkin <webestet@gmail.com>
2023-11-07 11:10:32 +0200

    Color indication fixed. Splitting of districts added

--- front/src/features/ads/api/ads.api.ts
+++ front/src/features/ads/api/ads.api.ts
@@ -13,6 +13,20 @@ class AdsApi extends BaseApi {
 
           df.fullDistrict = df.district;
 
+          // Обрабатываем специфические случаи
+          df.district = df.district.replace(
+            /Timiou Prodromou\s+Mesa Geitonias/g,
+            'Mesa Geitonia',
+          );
+
+          if (/\s+-\s+/.test(df.district)) {
+            // eslint-disable-next-line prefer-destructuring
+            df.district = df.district.split(/\s+-\s+/)[1];
+          }
+
+          // Удаляем "Tourist Area"
+          df.district = df.district.replace(/ Tourist Area/g, '');
+
           // Замена сокращений
           df.district = df.district
             .replace(/Ag /g, 'Ag. ')
@@ -22,23 +36,8 @@ class AdsApi extends BaseApi {
             .replace(/Ap /g, 'Ap. ')
             .replace(/Panag /g, 'Panag. ');
 
-          // Удаляем "Tourist Area"
-          df.district = df.district.replace(/ Tourist Area/g, '');
-
           // Обрабатываем скобки
           df.district = df.district.replace(/\((.*?)\)/g, ' - $1');
-
-          // Обрабатываем специфические случаи
-          df.district = df.district
-            .replace('Timiou Prodromou  Mesa Geitonias', 'Mesa Geitonia')
-            .replace('Strovolos - ', '')
-            .replace('Egkomi -', '');
-
-          // Заменяем тире с любым количеством пробелов вокруг на " - "
-          df.district = df.district.replace(/\s*-\s*/g, ' - ');
-
-          // Разделяем двойные районы
-          df.district = df.district.split(' - ').join();
 
           // Обрабатываем "Arch Makarios III"
           df.district = df.district.replace(
