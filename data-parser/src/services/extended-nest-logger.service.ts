f95895ce83317c99cb9f7386e3467272270a1f2b
Mikhail Filchushkin <webestet@gmail.com>
2023-11-25 16:59:28 +0200

    Log file generation fixed in Data Parser microservice

--- data-parser/src/services/extended-nest-logger.service.ts
+++ data-parser/src/services/extended-nest-logger.service.ts
@@ -33,7 +33,11 @@ export class ExtendedNestLogger extends NestLogger {
       timeRange = '18-00';
     }
 
-    return `${this.logPrefix}${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}--${timeRange}.log`;
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${this.logPrefix}${year}-${month}-${day}--${timeRange}.log`;
   }
 
   private deleteOutdatedLogs(): void {
